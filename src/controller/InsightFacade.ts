3945c98feb81204e2ee2e511af99658ab3b9cc7e
Yuntian Wan <wyuntian@dhcp-128-189-72-213.ubcsecure.wireless.ubc.ca>
2017-10-02 19:56:31 -0700

    first commit

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -2,8 +2,9 @@
  * This is the main programmatic entry point for the project.
  */
 import {IInsightFacade, InsightResponse} from "./IInsightFacade";
-
+var fs  = require('fs');
 import Log from "../Util";
+import * as jsz from 'jszip';
 
 export default class InsightFacade implements IInsightFacade {
 
@@ -12,7 +13,44 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-        return null;
+        return new Promise(function (fulfill, reject) {
+            //var zip = jsz;
+
+            // process the content (ie getting the info you want)
+            // store it into data structure
+            // save data structure to disk (using fs.writeFile)
+
+            let listFiles:any[] = [];
+            jsz.loadAsync(content, {'base64': true}).then(function(data){ // data is zipObject
+
+                // for(let eachFile of data) {
+                //     // fs.writeFile(eachFile);
+                //     console.log(eachFile);
+                // }
+                data.forEach(function(r,f){
+                    // console.log(f);
+                    f.async("string").then(function(c){
+                        console.log(c);
+
+
+                         if(c['result'].length != 0) {
+                             var newObj = {};
+                             //newObj('course_id');
+                             newObj['courses_dept'] = c["Title"];
+                             newObj['course_id'] = c["Course"];
+                             newObj[]
+
+                         }
+
+                        fulfill();
+                    })
+                    // fulfill();
+                })
+                //console.log(data);
+            }).catch(function(e){
+                reject(e);
+            });
+        });
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
--- test/EchoSpec.ts
+++ test/EchoSpec.ts
@@ -6,6 +6,7 @@ import Server from "../src/rest/Server";
 import {expect} from 'chai';
 import Log from "../src/Util";
 import {InsightResponse} from "../src/controller/IInsightFacade";
+import InsightFacade from "../src/controller/InsightFacade";
 
 describe("EchoSpec", function () {
 
@@ -63,6 +64,33 @@ describe("EchoSpec", function () {
         expect(out.code).to.equal(400);
         expect(out.body).to.have.property('error');
         expect(out.body).to.deep.equal({error: 'Message not provided'});
+    });
+
+    it("test addDataset", function () {
+       // var zip = new JSZip();
+        var fs = require('fs');
+       // var files:any[]=[];
+
+        var insight = new InsightFacade();
+        var fileName = '/Users/wyuntian/cpsc310_team70/courses.zip';
+
+        console.log('start file');
+        var content = fs.readFileSync(fileName);
+        content = content.toString("base64");
+
+        console.log(content);
+
+        insight.addDataset("courses", content).then(function (data) {
+            console.log(data);
+            expect(true).eq((true));
+
+
+        }).catch(function (err) {
+            console.log(err);
+
+        });
+        //expect(true).eq((true));
+
     });
 
 });
